9bdb6c13007dfc33e906e53119764e437ea1b6c2
Pogodin Eduard <pogodin.eduard.m@gmail.com>
2025-05-22 00:25:21 +0400

    WIP: first version of page layout

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,43 +1,70 @@
 import { Container } from "@/shared/ui/Container/Container";
-import styles from './App.module.scss';
-import menuIcon  from "@/shared/assets/icons/menu-alt-1.png";
+import styles from "./App.module.scss";
+import menuIcon from "@/shared/assets/icons/menu-alt-1.svg";
 import projectIcon from "@/shared/assets/icons/clipboard-list.png";
-import taskIcon from "@/shared/assets/icons/check-circle.png"
+import taskIcon from "@/shared/assets/icons/check-circle.png";
+import timer from "@/shared/assets/icons/timer.svg";
 
 const App = () => {
   return (
     <Container>
       <header className={styles.header}>
-        <img src={menuIcon} alt="Menu" className={styles.menuIcon} />
-        <h1 className={styles.title}>Auror Time</h1>
+        <img className={styles.icon} src={menuIcon} alt="Menu" />
+        <h1 className={styles.appTitle}>Auror Time</h1>
+        
       </header>
-      <main>
-        <nav className={styles.tabs}>
-          <button>Track</button>
-          <button>Tasks</button>
-          <button>Projects</button>
+      <nav className={styles.navBar}>
+          <ul className={styles.navList}>
+            <li className={styles.navItem}>
+              <button className={styles.navButton}>Track</button>
+            </li>
+            <li className={styles.navItem}>
+              <button className={styles.navButton}>Tasks</button>
+            </li>
+            <li className={styles.navItem}>
+              <button className={styles.navButton}>Projects</button>
+            </li>
+          </ul>
         </nav>
-        <section className={styles.section}>
-          <div className={styles.infoCards}>
-            <div className={styles.card}>
-              <img src={projectIcon} alt='Project'></img>
-              <span>
-                <div>Auror Time</div>
-                <div>Project</div>
-              </span>
-            </div>
-            <div className={styles.card}>
-              <img src={taskIcon} alt='Project'></img>
+
+      <main>
+        
+
+        <section className={styles.mainSection}>
+
+          <div className={styles.taskInfo}>
+            <div className={styles.taskCard}>
+              <img className={styles.icon} src={projectIcon} alt="Project"/>
               <div>
-                <div>Create UI</div>
-                <div>Task</div>
+                <p className={styles.taskTitle}>Auror Time</p>
+                <p className={styles.taskType}>Project</p>
+              </div>
+            </div>
+            <div className={styles.taskCard}>
+              <img className={styles.icon} src={taskIcon} alt="Task"/>
+              <div>
+                <p className={styles.taskTitle}>Create UI</p>
+                <p className={styles .taskType}>Task</p>
               </div>
             </div>
           </div>
-          
+
+          <div className={styles.timerWrapper}>
+            <div className={styles.timer}>
+              <div>
+                <div className={styles.timerDisplay}>00:00:00</div>
+                <div className={styles.taskTitle}>Create UI</div>
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.toggleWrapper}>
+            <div className={styles.toggleSwitch}>
+              <img className={styles.icon} src={timer} alt="switch"></img>
+              <p className={styles.toggleStatus}>Off</p>
+            </div>
+          </div>
         </section>
-       
-       
       </main>
     </Container>
   );
